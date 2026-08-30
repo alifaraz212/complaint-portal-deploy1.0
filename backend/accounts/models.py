970088aa4b257c0f6436f05310347ff74c248461
Ali Faraz <alifaraz@pk.folio3.com>
2026-08-30 05:11:23 +0500

    Fix custom UserManager, remove username field, add created_at

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,13 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
-from django.contrib.auth.models import AbstractUser
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+
+        email = self.normalize_email(email)
+
+        user = self.model(
+            email=email,
+            **extra_fields,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(
+            email,
+            password,
+            **extra_fields,
+        )
 
 
 class User(AbstractUser):
     class Role(models.TextChoices):
         USER = "user", "User"
         ADMIN = "admin", "Admin"
+
+    username = None
 
     email = models.EmailField(unique=True)
     full_name = models.CharField(max_length=150)
@@ -19,7 +47,11 @@ class User(AbstractUser):
     )
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username", "full_name"]
+    REQUIRED_FIELDS = ["full_name"]
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
