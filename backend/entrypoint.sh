f250d711b2f4843c9db6872a606f8c029254609f
Ali Faraz <alifaraz@pk.folio3.com>
2026-09-12 00:14:26 +0500

    Revert to clean state: remove unnecessary management command and simplify entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-set -e
-
-echo "Collecting static files..."
 python manage.py collectstatic --noinput
-
-echo "Running migrations..."
-python manage.py migrate
-
-echo "Entrypoint complete, starting gunicorn..."
 exec "$@"
